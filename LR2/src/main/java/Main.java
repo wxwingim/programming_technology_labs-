031ad5438794cbe73597261de6af5dec8076385e
wxwingim <78482070+wxwingim@users.noreply.github.com>
2021-09-27 17:58:56 +0300

    Add files via upload

--- LR2/src/main/java/Main.java
+++ LR2/src/main/java/Main.java
@@ -2,38 +2,37 @@ public class Main {
     public static void main(String[] args) {
 
         // массив участников
-        Movement[] creature = new Movement[3];
-        creature[0] = new Human();
-        creature[1] = new Robot();
-        creature[2] = new Cat();
+        Movement[] creature = {
+                new Human(),
+                new Robot(),
+                new Cat()
+        };
 
         // массив препятствий
         Let[] lets = new Let[6];
-        for (int i =0; i< lets.length; i++){
-            lets[i] = Let.values()[i];
+        for (Let o: lets) {
+            lets = Let.values();
         }
 
         // прохождение
-        for (int i = 0; i< creature.length; i++){
-            for (int j = 0; j < lets.length; j+=2){
-                if (lets[j].getSize() <= creature[i].getMaxRun()){
-                    creature[i].run(lets[j].getSize());
-                }
-                else {
-                    creature[i].run(lets[j].getSize());
+        for (Movement create: creature) {
+            for (int j = 0; j < lets.length; j+=2) {
+                if (lets[j].getSize() <= create.getMaxRun()) {
+                    create.run(lets[j].getSize());
+                } else {
+                    create.run(lets[j].getSize());
                     System.out.println("Выбыл с полосы препятствий.");
-                    i++;
+
                     j = -2;
                     break;
                 }
-                if (lets[j+1].getSize() <= creature[i].getMaxJump()){
-                    creature[i].jump(lets[j+1].getSize());
-                }
-                else {
-                    creature[i].jump(lets[j+1].getSize());
+                if (lets[j + 1].getSize() <= create.getMaxJump()) {
+                    create.jump(lets[j + 1].getSize());
+                } else {
+                    create.jump(lets[j + 1].getSize());
                     System.out.println("Выбыл с полосы препятствий.");
-                    i++;
                     j = -2;
+                    break;
                 }
                 if (j + 2 == lets.length) {
                     System.out.println("Полоса препятствий пройдена.");
